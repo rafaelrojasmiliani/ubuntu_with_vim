afa12fc031bffef09f432ffc6490ca10d9edd508
Rafael Rojas <rojas@imfusion.com>
2023-01-13 10:44:01 +0100

    Add ROS rqt publisher, service called and tf tree

--- configfiles/install_ros_packages.bash
+++ configfiles/install_ros_packages.bash
@@ -12,30 +12,33 @@ main(){
     && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends -o Dpkg::Options::="--force-confnew" \
                     python3-catkin-tools \
                     ros-$distro-control-msgs \
-                    ros-$distro-velocity-controllers \
-                    ros-$distro-joint-trajectory-action \
-                    ros-$distro-ifopt \
-                    ros-$distro-plotjuggler \
-                    ros-$distro-joint-trajectory-controller \
-                    ros-$distro-joint-trajectory-action \
-                    ros-$distro-xacro \
                     ros-$distro-gazebo-ros \
                     ros-$distro-gazebo-ros-control \
+                    ros-$distro-ifopt \
                     ros-$distro-joint-state-controller \
+                    ros-$distro-joint-state-publisher \
+                    ros-$distro-joint-trajectory-action \
+                    ros-$distro-joint-trajectory-action \
+                    ros-$distro-joint-trajectory-controller \
+                    ros-$distro-jsk-rviz-plugins \
+                    ros-$distro-moveit-msgs \
+                    ros-$distro-pcl-ros \
+                    ros-$distro-plotjuggler \
+                    ros-$distro-plotjuggler-ros \
                     ros-$distro-position-controllers \
                     ros-$distro-robot-state-publisher \
-                    ros-$distro-joint-state-publisher \
+                    ros-$distro-roslint \
                     ros-$distro-rqt \
                     ros-$distro-rqt-graph \
-                    ros-$distro-roslint \
-                    ros-$distro-plotjuggler-ros \
                     ros-$distro-rqt-gui \
                     ros-$distro-rqt-gui-py \
-                    ros-$distro-rqt-py-common \
-                    ros-$distro-moveit-msgs \
                     ros-$distro-rqt-joint-trajectory-controller \
-                    ros-$distro-jsk-rviz-plugins \
-                    ros-$distro-pcl-ros
+                    ros-$distro-rqt-publisher\
+                    ros-$distro-rqt-py-common \
+                    ros-$distro-rqt-service-caller \
+                    ros-$distro-rqt-tf-tree \
+                    ros-$distro-velocity-controllers \
+                    ros-$distro-xacro
     if ! dpkg --verify ros-$distro-moveit-ros 2>/dev/null; then
         apt-get update \
         && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends -o Dpkg::Options::="--force-confnew" \

0b6bc7e0d6fa79296e44ca099ea70e3bd9e87e5f
Rafael Rojas <rojas@imfusion.com>
2023-04-14 15:19:33 +0200

    Allow to install firefox

--- configfiles/install_ubuntu_base.bash
+++ configfiles/install_ubuntu_base.bash
@@ -248,6 +248,18 @@ main() {
         source /workspace/configfiles/install_ifopt.bash
         install_ifopt
     fi
+
+    # --- Allow to install firefox
+
+    add-apt-repository ppa:mozillateam/ppa
+    echo '
+    Package: *
+    Pin: release o=LP-PPA-mozillateam
+    Pin-Priority: 1001
+    ' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+
+    echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' |
+        sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
 }
 
 main

37cf465ad1fbf1eae6a02218a7f802156fa1603a
Rafael Rojas <rojas@imfusion.com>
2023-08-21 20:06:46 +0200

    Update ros1 installation

--- configfiles/install_ros_packages.bash
+++ configfiles/install_ros_packages.bash
@@ -20,8 +20,7 @@ main() {
 
         echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -cs) main" \
             >/etc/apt/sources.list.d/ros1-latest.list
-        apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 \
-            --recv-keys C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+        curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
         apt-get update
 
         DEBIAN_FRONTEND=noninteractive apt-get install \

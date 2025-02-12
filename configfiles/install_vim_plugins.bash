4f462b1d41c9103fb528b2b291b6ec3d27a0213c
Rafael Rojas <rojas@imfusion.com>
2025-02-12 11:40:38 +0100

    Add llama cpp only with vim >=9.1

--- configfiles/install_vim_plugins.bash
+++ configfiles/install_vim_plugins.bash
@@ -8,14 +8,18 @@ plugins=(
     https://github.com/jpalardy/vim-slime.git
     https://github.com/ludovicchabant/vim-gutentags.git
     https://github.com/skywind3000/gutentags_plus.git
-    https://github.com/ggml-org/llama.vim.git
 )
+# Check Vim version
+vim_version=$(vim --version | awk 'NR==1 {print $5}')
+if awk -v v="$vim_version" 'BEGIN {exit !(v >= 9.1)}'; then
+    plugins+=("https://github.com/ggml-org/llama.vim.git")
+fi
 
 main() {
     set -xeu
     DISTRIB_RELEASE=$(lsb_release -sr 2>/dev/null)
     for plugin in ${plugins[@]}; do
-        git clone --recurse-submodules -j8 $plugin \
+        git clone --recurse-submodules -j$(nproc) $plugin \
             /etc/vim/bundle/$(echo $(basename $plugin) | sed 's/\.git//')
     done
 
